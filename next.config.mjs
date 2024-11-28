4f2ea13d420d5db413e7452131d8e6d1ae7d6cb0
raiyan ahmed <raiyan.c.me@gmail.com>
2024-11-27 23:12:31 -0800

    config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	async rewrites() {
-		return [
-			{
-				source: "/api/:path*",
-				destination: "https://localhost:5959/api/:path*",
-			},
-		];
-	},
+	// async rewrites() {
+	// 	return [
+	// 		{
+	// 			source: "/api/:path*",
+	// 			destination: "https://localhost:5959/api/:path*",
+	// 		},
+	// 	];
+	// },
 };
 
 export default nextConfig;
